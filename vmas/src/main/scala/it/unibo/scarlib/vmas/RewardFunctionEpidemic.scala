5ff88d25c6531df6767e66621e83abdd8536e0ce
SycamoreQ <kaushik.80405@gmail.com>
2025-08-17 22:34:21 +0530

    fix(vmas): resolve merge conflict in RewardFunctionEpidemic.scala

--- vmas/src/main/scala/it/unibo/scarlib/vmas/RewardFunctionEpidemic.scala
+++ vmas/src/main/scala/it/unibo/scarlib/vmas/RewardFunctionEpidemic.scala
@@ -17,20 +17,61 @@ object RewardFunctionEpidemic {
   private var rf: Option[RewardFunction] = None
 
   abstract class RewardFunctionStep(param: RewardFunctionStepParam) {
-    def compute()(implicit currentState: State, action: model.Action, newState: State): Any
+    def compute()(implicit currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): Any
   }
 
   def rewardFunctionStep(init: => RewardFunctionStep): Unit = {
     rf = Option(new RewardFunction {
-      override def compute(currentState: State, action: model.Action, newState: State): Double = init.compute()(currentState, action, newState).asInstanceOf[Double]
+      override def compute(currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): Double = init.compute()(currentState.asInstanceOf[EpidemicState], action.asInstanceOf[EpidemicAction], newState.asInstanceOf[EpidemicState]).asInstanceOf[Double]
     })
   }
 
   trait RewardFunctionStepParam
-  case object Current extends RewardFunctionStepParam
-  case object Action extends RewardFunctionStepParam
-  case object New extends RewardFunctionStepParam
 
+  case object Prev extends RewardFunctionStepParam
+
+  case object Action extends RewardFunctionStepParam
+
+  case object Current extends RewardFunctionStepParam
+
+
+  case class Tensor(x: py.Dynamic, stringed: String)
+
+  object Tensor {
+    def apply(x: Any): Tensor = new Tensor(py.eval(s"torch.tensor($x)"), s"torch.tensor($x)")
+  }
+
+  case class Lambda(x: py.Dynamic, stringed: String)
+
+  object Lambda {
+    def apply(x: Any): Lambda = new Lambda(py.eval("lambda " + x.toString), "lambda " + x.toString)
+  }
+
+  implicit def tensorToPyDynamic(x: Tensor): (py.Dynamic, String) = (x.x, x.stringed)
+
+  implicit def doubleToPyDynamic(x: Double): (py.Dynamic, String) = (py.eval(x.toString), x.toString)
+
+  implicit def lambdaToPyDynamic(x: Lambda): (py.Dynamic, String) = (x.x, x.stringed)
+
+  def asTensor(values: Seq[Double]): py.Dynamic = py.module("torch").tensor(py.Dynamic)
+
+  case class InfectionPenalty(param: RewardFunctionStepParam) extends RewardFunctionStep(param) {
+    override def compute()(implicit currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): py.Dynamic = {
+      val rewardValue: Double = newState.getInfectionRate - currentState.getInfectionRate * -1.0
+
+      // Wrap the double reward into a Tensor (i.e., PyTorch tensor + string representation)
+      val rewardTensor = Tensor(rewardValue) // uses Tensor.apply(double)
+
+      // Return the py.Dynamic representation (the actual tensor) to integrate with PyTorch pipeline
+      rewardTensor.x
+    }
+
+    case class hospitalUtilization(param: RewardFunctionStepParam) extends RewardFunctionStep(param) {
+      override def compute()(implicit currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): Any = {
+        val rewardValue: Double = if (currentState.getHospitalUtilization > 1.0) -200.0 else (-currentState.getHospitalUtilization * 100)
+      }
+    }
+  }
 
 
 }
