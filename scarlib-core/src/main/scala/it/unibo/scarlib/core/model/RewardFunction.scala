109a239c00e6499d2b09c38a75fd4f1337d75a0d
SycamoreQ <kaushik.80405@gmail.com>
2025-08-19 15:22:54 +0530

    fix(scarlib): Removed original

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/RewardFunction.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/RewardFunction.scala
@@ -15,65 +15,7 @@ import it.unibo.scarlib.core.model.EpidemicState
 /** The function that evaluates the action performed by an agent in a certain state */
 trait RewardFunction {
 
-  def compute(currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): Double = {
-
-    var reward = 0.0
-    (currentState, newState) match {
-      case (prev: EpidemicState, curr: EpidemicState) => {
-        def infectionPenalty(): Double =
-          (curr.getInfected() - prev.getInfected()) * -1.0
-
-        def hospitalPenalty(): Double = {
-          if (curr.getHospitalUtilization() > 1.0) -200.0
-          else -curr.getHospitalUtilization() * 100
-        }
-
-        def vaccinationBonus(): Double =
-          (curr.getVaccinationRate() - prev.getVaccinationRate()) * 500
-
-        def airportfunc(diseasecountry: EpidemicState, targetcountry: Seq[EpidemicState]): Double = {
-          val migrationinfo = curr.getMostConnectedCountries(diseasecountry, targetcountry)
-
-          migrationinfo match {
-            case migrationinfo.headOption => reward += curr.getInfectionRate * 100
-            case _ => reward += curr.getInfectionRate * 10
-          }
-          reward
-        }
-
-        def HospitalCause(): Double = {
-          val hCapacity = curr.hospitalCapacity
-
-          hCapacity match {
-            case hCapacity > 10000 => reward += curr.getInfectionRate * 100
-            case _ => reward += curr.getInfectionRate * 2
-          }
-          reward
-        }
-
-        def VaccinationDrive(): Double = {
-          val vaccinated = curr.vaccinatedPopulation
-
-          vaccinated match {
-            case vaccinated > curr.infected => reward -= curr.getVaccinationRate * 10
-            case vaccinated > curr.incomingTravelers => (reward -= curr.getVaccinationRate * 100)
-            case vaccinated > curr.outgoingTravelers => reward -= curr.getVaccinationRate * 10
-            case _ => reward += curr.getVaccinationRate * 10
-          }
-          reward
-        }
-
-        def geoLoc(diseaseCountry: Seq[EpidemicState] , neighbours : Seq[EpidemicState]): Double = {
-          val spread: Seq[Double] = neighbours.map(_.radiusOfAffect(diseaseCountry, neighbours))
-
-          spread match {
-            case spread < 10 => reward += curr.getInfectionRate * 100
-            case _ => reward += curr.getInfectionRate * 10
-          }
-        }
-      }
-    }
-  }
+  def compute(currentState: EpidemicState, action: EpidemicAction, newState: EpidemicState): Double
 }
 
 
